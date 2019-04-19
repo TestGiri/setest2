e5a8a0477e0bd7975e5b88c86a6c1cc73b973c0d
TestGiri <girish.bdvt23@gmail.com>
2019-04-19 16:31:17 +0530

    version1

---
+++ google_searchtest.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import unittest
+class Amazonsearch(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(
+            executable_path="C:\\Users\\Test\\Downloads\\chromedriver_win32\\chromedriver.exe")
+        cls.driver.implicitly_wait(20)
+        cls.driver.maximize_window()
+
+    def test_serach_drivesearch(self):
+        self.driver.get("https://amazon.in")
+        self.driver.find_element_by_name("field-keywords").send_keys("ssd external drives offer")
+        self.driver.find_element_by_xpath("//*[@id='nav-search']/form/div[2]/div/input").click()
+        self.driver.back()
+
+    @classmethod
+    def tearDown(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("test completed")
+
+if __name__ == '__main__':
+    unittest.main()
